02932c5dde92b6cb9079c0ed790477b1a2617d8e
MrKravz <4otkiipozzik44@gmail.com>
2026-07-01 16:48:59 +0300

    init commit

---
+++ PathListService/src/main/java/by/ares/path_list_service/PathListServiceApplication.java
@@ -0,0 +1,13 @@
+package by.ares.path_list_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathListServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathListServiceApplication.class, args);
+    }
+
+}
