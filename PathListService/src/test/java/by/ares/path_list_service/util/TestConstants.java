6f9087173f365414dfb839ec49d8a124aebfdb3e
MrKravz <4otkiipozzik44@gmail.com>
2026-07-13 15:23:24 +0300

    refactoring: updated db schema

--- PathListService/src/test/java/by/ares/path_list_service/util/TestConstants.java
+++ PathListService/src/test/java/by/ares/path_list_service/util/TestConstants.java
@@ -22,7 +22,6 @@ public class TestConstants {
     public static final Long EXISTING_COMPANY_ID = 1L;
     public static final String COMPANY_IDENTIFIER = "1147847423899";
     public static final String COMPANY_NAME = "Strathen Oakment";
-    public static final String COMPANY_FULL_NAME = "Jordan Belfort";
     public static final String COMPANY_ADDRESS = "Wall st., h. 3";
     public static final String COMPANY_PHONE_NUMBER = "+375291111111";
     public static final UUID EXISTING_PATH_LIST_ID = UUID.randomUUID();
