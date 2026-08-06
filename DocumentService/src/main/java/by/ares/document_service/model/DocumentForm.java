3742d193e95127a7035cb743dd4592b4cdb0f5de
MrKravz <4otkiipozzik44@gmail.com>
2026-08-06 18:15:36 +0300

    feature: updated schema

---
+++ DocumentService/src/main/java/by/ares/document_service/model/DocumentForm.java
@@ -0,0 +1,9 @@
+package by.ares.document_service.model;
+
+public enum DocumentForm {
+    FORM_4P,
+    FORM_4S,
+    FORM_6,
+    FORM_1,
+
+}
--- DocumentService/src/main/java/by/ares/document_service/model/TransportationType.java
+++ DocumentService/src/main/java/by/ares/document_service/model/TransportationType.java
@@ -1,8 +1,8 @@
 package by.ares.document_service.model;
 
 public enum TransportationType {
-    URBAN,
-    SUBURBAN,
-    INTERCITY ,
-    INTERNATIONAL
+    PERSONAL,
+    ORDERED,
+    REGULAR,
+    TAXI
 }
