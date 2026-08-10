28d71983199a74fd9e20c6392250dc64d1c8e387
MrKravz <4otkiipozzik44@gmail.com>
2026-08-10 12:55:24 +0300

    feature: flexible new template integration

--- DocumentService/src/main/java/by/ares/document_service/model/DocumentForm.java
+++ DocumentService/src/main/java/by/ares/document_service/model/DocumentForm.java
@@ -1,9 +1,30 @@
 package by.ares.document_service.model;
 
-public enum DocumentForm {
-    FORM_4P,
-    FORM_4S,
-    FORM_6,
-    FORM_1,
+import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Getter
+public enum DocumentForm {
+
+    FORM_4P("4-П"),
+    FORM_4S("4-С"),
+    FORM_6("6"),
+    FORM_1("1");
+
+    private final String formName;
+
+    DocumentForm(String formName) {
+        this.formName = formName;
+    }
+
+    private static final Map<String, DocumentForm> LOOKUP_MAP = Arrays.stream(values())
+            .collect(Collectors.toMap(DocumentForm::getFormName, Function.identity()));
+
+    public static DocumentForm fromString(String formName) {
+        return LOOKUP_MAP.get(formName);
+    }
 }
